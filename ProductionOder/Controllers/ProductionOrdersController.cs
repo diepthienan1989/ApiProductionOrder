0b2a46adf72156e51786c576226e0e9496422fb4
diepthienan1989 <80557652+diepthienan1989@users.noreply.github.com>
2023-12-22 16:42:42 +0700

    2

---
+++ ProductionOder/Controllers/ProductionOrdersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductionOrder.Data;
+using ProductionOrder.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace ProductionOrder.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductionOrdersController : Controller
+    {
+        private readonly AESProductionOrdersDbContext _ProductionOderDbContext;
+        public ProductionOrdersController(AESProductionOrdersDbContext ProductionOderDbContext)
+        {
+            _ProductionOderDbContext = ProductionOderDbContext;
+        }
+
+        [HttpPost(Name = "NewProductionOrder")]
+        public async Task<IActionResult> AddData([FromBody] ProductionOrders dataRequest)
+        {
+            await _ProductionOderDbContext.ProductionOrders.AddAsync(dataRequest);
+            await _ProductionOderDbContext.SaveChangesAsync();
+            return Ok(dataRequest);
+
+        }
+
+        [HttpGet]
+        [Route("{id:long}")]
+        public async Task<IActionResult> GetDataId([FromRoute] Guid id)
+        {
+            int ID = 0;
+            var data = await _ProductionOderDbContext.ProductionOrders.FirstOrDefaultAsync(x => x.ProductionOrderID == id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return Ok(data);
+        }
+
+      
+    }
+}
---
+++ ProductionOder/Data/AESProductionOrdersDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ProductionOrder.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace ProductionOrder.Data
+
+{
+    public class AESProductionOrdersDbContext : DbContext
+    {
+        public AESProductionOrdersDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<ProductionOrders> ProductionOrders { get; set; }
+    }
+}
